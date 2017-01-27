151454b4235141133b3a64b89e479290c3db8075
shoma <shoma@mercari.com>
2017-01-27 16:45:00 +0900

    Change to use flask jsonify

--- service.py
+++ service.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
-import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return json.dumps(dict(os.environ), sort_keys=True, indent=4)
+    return jsonify(dict(os.environ))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080', debug=None)
