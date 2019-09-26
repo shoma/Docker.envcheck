d8ee3cc1f782180a3815ff79fe109ad8944bb389
shoma <shoma@mercari.com>
2019-09-26 15:20:14 +0900

    Add pipfile scripts

--- service.py
+++ service.py
@@ -3,9 +3,11 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return jsonify(dict(os.environ))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080', debug=False)
